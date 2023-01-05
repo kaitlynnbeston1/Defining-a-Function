ca9313352f6561950fda5f4120ed8a20bc9ed473
kaitlynnbeston1 <113719685+kaitlynnbeston1@users.noreply.github.com>
2023-01-05 08:51:50 -0600

    ...

---
+++ favoriteBooks.py
@@ -0,0 +1,11 @@
+# Defining our function
+def favorite_books(book):
+    """Displays the user's favorite book. """
+    print(f"One of my favorite books is {book.title()}.")
+
+
+# Testing it out with some of my favorites
+favorite_books("Harry Potter and the Prisoner of Azkaban")
+favorite_books("Verona Comics")
+favorite_books("The Hate U give")
+favorite_books("Because You'll Never Meet Me")
---
+++ message.py
@@ -0,0 +1,14 @@
+# Defining our function
+def message(chapter):
+    """Tells the user what the chapter is that we are learning about. """
+    print(f"In this chapter, we are learning about {chapter}.")
+
+# Telling the user about the chapter
+message("functions")
+
+
+# Telling the user about fake chapters because I wanna test some more. 
+message("cats")
+message("Python")
+message("Undertale")
+message("psychokinesis")
